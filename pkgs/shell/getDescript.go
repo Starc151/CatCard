1c9041158ece5f6a24952eb81f2a06e7ea5faff6
Starc151 <dr.Starc@ya.ru>
2024-01-09 13:20:59 +0300

    description scroll

--- pkgs/shell/getDescript.go
+++ pkgs/shell/getDescript.go
@@ -9,17 +9,23 @@ import (
 )
 
 func (sh *Shell) getDescript(numRow int) *fyne.Container {
+	sh.card.descript = append(sh.card.descript, &canvas.Text{})
 	vBox := container.NewVBox()
 	excelFileName := "./slu/data.xlsx"
     xlFile, _ := xlsx.OpenFile(excelFileName)
 	firstRow := xlFile.Sheets[0].Rows[0].Cells
 	for numCell := 0; numCell <= len(firstRow)-1; numCell++ {
-		descript := canvas.Text{}
-		descript.Text = firstRow[numCell].String()+" "+xlFile.Sheets[0].Rows[numRow].Cells[numCell].String()
-		descript.TextSize = 20
-		vBox.Add(&descript)
+		if (firstRow[numCell].String() != "") && (xlFile.Sheets[0].Rows[numRow].Cells[numCell].String() != "") {
+			sh.card.descript[numCell] = &canvas.Text{}
+			sh.card.descript[numCell].Text = firstRow[numCell].String()+" "+xlFile.Sheets[0].Rows[numRow].Cells[numCell].String()
+			sh.card.descript[numCell].TextSize = 20
+			sh.card.descript = append(sh.card.descript, sh.card.descript[numCell])
+			vBox.Add(sh.card.descript[numCell])
+		}
 	}
-	layout := container.NewWithoutLayout(vBox)
+	vScroll := container.NewVScroll(vBox)
+	vScroll.Resize(fyne.NewSize(416, 450))
+	layout := container.NewWithoutLayout(vScroll)
 	layout.Move(fyne.NewPos(470, 100))
 	return layout
 }
