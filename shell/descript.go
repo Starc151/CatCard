9f31a7ce6314bc3e13a31fb45660499e111dcf12
Starc151 <dr.Starc@ya.ru>
2024-01-19 07:53:29 +0300

    fix list

--- shell/descript.go
+++ shell/descript.go
@@ -1,8 +1,6 @@
 package shell
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
@@ -14,29 +12,20 @@ func (sh *Shell) descript() *fyne.Container {
 	noDiscript := canvas.NewText("Нет данных", nil)
 	noDiscript.TextSize = 20
 	rows, _ := sh.xlFile.GetRows(sh.catalogName)
-	firstRow := rows[0]
+
+	vBox.Add(noDiscript)
 	
-	if sh.id <= len(rows) {
+	if (sh.id <= len(rows)) && (len(rows[sh.id-1]) > 0) {
+		vBox.RemoveAll()
 		for numCell, cell := range rows[sh.id-1] {
-			// if rows[sh.id-1] != nil {
-				sh.descriptCard[numCell] = &canvas.Text{}
-				sh.descriptCard[numCell].TextSize = 20
-				sh.descriptCard[numCell].Text = firstRow[numCell]+" "+cell
-				if cell != "" {
-					sh.descriptCard = append(sh.descriptCard, sh.descriptCard[numCell])
-					vBox.Add(sh.descriptCard[numCell])
-				}
-			// } else {
-				if len(rows[sh.id-1]) == 0 {
-					fmt.Println(len(rows[sh.id-1]))
-					// vBox.Add(noDiscript)
-				}
-			// 	fmt.Println(len(rows[sh.id-1]))
-			// 	vBox.Add(noDiscript)
-			// }
+			sh.descriptCard[numCell] = &canvas.Text{}
+			sh.descriptCard[numCell].TextSize = 20
+			sh.descriptCard[numCell].Text = rows[0][numCell]+" "+cell
+			if cell != "" {
+				sh.descriptCard = append(sh.descriptCard, sh.descriptCard[numCell])
+				vBox.Add(sh.descriptCard[numCell])
+			}
 		}
-	} else {
-		vBox.Add(noDiscript)
 	}
 
 	layout := container.NewWithoutLayout(vScroll(416, 450, vBox))
