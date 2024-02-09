e8b9cbcb64182234a114ebcf7de93404d0035b00
Starc151 <dr.Starc@ya.ru>
2024-02-09 12:48:48 +0300

    fix search

--- shell/btns.go
+++ shell/btns.go
@@ -70,14 +70,15 @@ func (sh *Shell) search(request string) {
 	result := map[string][]int{}
 	for _, listNane := range sh.xlFile.GetSheetList() {
 		rows, _ :=  sh.xlFile.GetRows(listNane)
-		for _, row := range rows {
-			for numRow, cell := range row {
-				if request == cell {
+		for numRow, row := range rows {
+			for _, cell := range row {
+				if request == cell && numRow != 0 {
 					result[listNane] = append(result[listNane], numRow + 1)
 				}
 			}
 		}
 	}
+	
 	sh.lenCatalog = len(result)
 	catalogNameText := canvas.NewText("Результаты поиска", nil)
 	if len(result) == 0 {
@@ -87,34 +88,35 @@ func (sh *Shell) search(request string) {
 	catalogName.Resize(fyne.NewSize(890, 70))
 	catalogName.Move(fyne.NewPos(10, 20))
 	cont := container.NewWithoutLayout(sh.searchBox(), catalogName)
-
-	// gridColumns := container.NewGridWithColumns(4)
-	// for numPic := 2; numPic <= sh.lenCatalog+1; numPic++{
-	// 	pic := sh.pic(fmt.Sprintf("pics/%s/%d.jpg", sh.catalogName, numPic))
-	// 	id := numPic
-	// 	pic.SetMinSize(fyne.NewSize(195, 109))
-	// 	btn := sh.button(
-	// 		"", 195, 109,
-	// 		0, 0, func() {
-	// 			sh.id = id
-	// 			sh.showCard()
-	// 		},
-	// 	)
-	// 	contCard := container.New(
-	// 		layout.NewStackLayout(),
-	// 		btn,
-	// 		pic,
-	// 	)
-	// 	contCard.Resize(fyne.NewSize(195, 109))
-	// 	gridColumns.Add(container.NewWithoutLayout(contCard))
-	// }
 	
-	// vScroll := container.NewVScroll(gridColumns)
-	// vScroll.Resize(fyne.NewSize(890, 460))
-	// vScroll.Move(fyne.NewPos(10, 80))
-	// cont.Add(vScroll)
-		fmt.Println(sh.lenCatalog)
-
-
+	gridColumns := container.NewGridWithColumns(4)
+	for listName, ids := range result {
+		catalogName := listName
+		for _, numStr := range ids {
+			pic := sh.pic(fmt.Sprintf("pics/%s/%d.jpg", listName, numStr))
+			id := numStr
+			pic.SetMinSize(fyne.NewSize(195, 109))
+			btn := sh.button(
+				"", 195, 109,
+				0, 0, func() {
+					sh.catalogName = catalogName
+					sh.id = id
+					sh.showCard()
+				},
+			)
+			contCard := container.New(
+				layout.NewStackLayout(),
+				btn,
+				pic,
+			)
+			contCard.Resize(fyne.NewSize(195, 109))
+			gridColumns.Add(container.NewWithoutLayout(contCard))
+		}
+	}
+	
+	vScroll := container.NewVScroll(gridColumns)
+	vScroll.Resize(fyne.NewSize(890, 460))
+	vScroll.Move(fyne.NewPos(10, 80))
+	cont.Add(vScroll)
 	sh.setContent(cont)
 }
